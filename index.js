c0807ddc357059a7102489e127a6ffadff6bd68c
Sam Waters <samwatersw15@gmail.com>
2019-04-17 21:07:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,8 @@ function appendKitten(name) {
   var kittenz = kittens.concat(name)
   return kittenz
 }
+
+function prependKitten(name) {
+  var kittenz = name.concat(kittens)
+  return kittenz
+}
